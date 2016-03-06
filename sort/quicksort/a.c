1bd8ddad45f2013752af8482ef5b865c74dfb8a1
David Sun <iamslash@gmail.com>
2016-03-06 17:25:13 +0900

    added lomuto partition scheme

--- sort/quicksort/a.c
+++ sort/quicksort/a.c
@@ -1,7 +1,4 @@
 
-/* Time Complexity */
-/* - Average : O(NlgN) */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +6,7 @@
 void rand_array(int a[])
 {
     for (int i=0; i<10; ++i)
-        a[i] = rand() % 10;    
+        a[i] = rand() % 100;    
 }
 
 void print_array(int a[])
@@ -19,7 +16,10 @@ void print_array(int a[])
     printf("\n");
 }
 
-void quickSort(int arr[], int left, int right) {
+/* Hoare partition scheme  */
+/* Time Complexity */
+/* - Average : O(NlgN) */
+void quick_sort_1(int arr[], int left, int right) {
     int i = left, j = right;
     int tmp;
     int pivot = arr[(left + right) / 2];
@@ -41,9 +41,40 @@ void quickSort(int arr[], int left, int right) {
  
     /* recursion */
     if (left < j)
-        quickSort(arr, left, j);
+        quick_sort_1(arr, left, j);
     if (i < right)
-        quickSort(arr, i, right);
+        quick_sort_1(arr, i, right);
+}
+
+/* Lomuto partition scheme */
+/* Time Complexity */
+/* - Average : O(NlgN) */
+void swap(int A[], int s, int d)
+{
+    int t = A[s];
+    A[s]  = A[d];
+    A[d]  = t;
+}
+int partition(int A[], int l, int r)
+{
+    int val_pivot = A[r];
+    int i = l;
+
+    for (int j=l; j<r; ++j)
+        if (A[j] < val_pivot)
+            swap(A, j, i++);
+    swap(A, i, r);
+    
+    return i;
+}
+void quick_sort_2(int A[], int l, int r)
+{
+    // base condition
+    if (l >= r) return;
+
+    int p = partition(A, l, r);
+    quick_sort_2(A, l, p-1);
+    quick_sort_2(A, p+1, r);
 }
 
 int main()
@@ -53,7 +84,6 @@ int main()
     rand_array(N);
     print_array(N);
     
-    quickSort(N, 0, 9);
+    quick_sort_2(N, 0, 9);
     print_array(N);
-    
 }
