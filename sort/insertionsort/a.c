9e43d3511acb2ea6c2f043bc2838373ea3191968
David Sun <iamslash@gmail.com>
2016-03-07 13:57:42 +0900

    insertion sort kick off

---
+++ sort/insertionsort/a.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void rand_array(int A[])
+{
+    for (int i=0; i<10; ++i)
+        A[i] = rand() % 100;    
+}
+
+void print_array(int A[], int t)
+{
+    for (int i=0; i<t; ++i)
+        printf("\t");
+    for (int i=0; i<10; ++i)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+void swap(int A[], int s, int d)
+{
+    int t = A[s];
+    A[s]  = A[d];
+    A[d]  = t;
+}
+/* Bubble Sort */
+/* Time Complexity : O(N^2) */
+void insertion_sort(int A[], int len)
+{
+}
+
+int main()
+{
+    int N[10] = {1, 5, 4, 3, 2, 10, 7, 6, 8, 9};
+
+    rand_array(N);
+    print_array(N, 0);
+    
+    insertion_sort(N, 10);
+    print_array(N, 0);
+}
