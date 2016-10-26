74d4c964dcd3e1a04053740af7db8e46590eb5c0
iamslash <iamslash@gmail.com>
2016-10-26 17:05:29 +0900

    added asciitree

--- tree/treap/a.c
+++ tree/treap/a.c
@@ -13,24 +13,32 @@ main( )
 
     T = Initialize( );
     T = MakeEmpty( NullNode );
-    for( i = 0; i < NumItems; i++, j = ( j + 7 ) % NumItems )
+    /* for( i = 0; i < NumItems; i++, j = ( j + 7 ) % NumItems ) */
+    /*     T = Insert( j, T ); */
+    /* for( i = 0; i < NumItems; i++ ) */
+    /*     if( ( P = Find( i, T ) ) == NullNode || Retrieve( P ) != i ) */
+    /*         printf( "Error1 at %d\n", i ); */
+
+    /* for( i = 0; i < NumItems; i += 2 ) */
+    /*     T = Remove( i, T ); */
+
+    /* for( i = 1; i < NumItems; i += 2 ) */
+    /*     if( ( P = Find( i, T ) ) == NullNode || Retrieve( P ) != i ) */
+    /*        printf( "Error2 at %d\n", i ); */
+
+    /* for( i = 0; i < NumItems; i += 2 ) */
+    /*     if( ( P = Find( i, T ) ) != NullNode ) */
+    /*         printf( "Error3 at %d\n", i ); */
+    /* printf( "Min is %d, Max is %d\n", Retrieve( FindMin( T ) ), */
+    /*            Retrieve( FindMax( T ) ) ); */
+
+
+    const int NumItemsSmall = 100;
+    for( i = 0; i < NumItemsSmall; i++, j = ( j + 7 ) % NumItemsSmall )
         T = Insert( j, T );
-    for( i = 0; i < NumItems; i++ )
-        if( ( P = Find( i, T ) ) == NullNode || Retrieve( P ) != i )
-            printf( "Error1 at %d\n", i );
-
-    for( i = 0; i < NumItems; i += 2 )
-        T = Remove( i, T );
-
-    for( i = 1; i < NumItems; i += 2 )
-        if( ( P = Find( i, T ) ) == NullNode || Retrieve( P ) != i )
-           printf( "Error2 at %d\n", i );
-
-    for( i = 0; i < NumItems; i += 2 )
-        if( ( P = Find( i, T ) ) != NullNode )
-            printf( "Error3 at %d\n", i );
-    printf( "Min is %d, Max is %d\n", Retrieve( FindMin( T ) ),
-               Retrieve( FindMax( T ) ) );
+    
+    // pretty print tree
+    PrintTree(T);
 
     return 0;
 }
