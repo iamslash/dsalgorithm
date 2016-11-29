4a14b89d004bdf0bead14a6df1f0ef8111d78a01
iamslash <iamslash@gmail.com>
2016-11-29 16:16:36 +0900

    ahocorasic ing...

--- tree/ahocorasic/a.cpp
+++ tree/ahocorasic/a.cpp
@@ -1,6 +1,10 @@
 // Copyright (C) 2016 by iamslash
 
 #include <vector>
+#include <cstring>
+#include <string>
+#include <map>
+#include <queue>
 
 const int ALPHABETS = 26;
 int ToNumber(char ch) {
@@ -10,9 +14,17 @@ int ToNumber(char ch) {
 // node of trie
 struct TrieNode {
   TrieNode* children[ALPHABETS];
-  // this node is terminal
-  bool terminal;
-  TrieNode() : terminal(false) {
+  // 현 위치에서 끝나는 문자열의 번호
+  int terminal;
+  // 실패 연결: 이 노드에서 매칭이 실패했을 때 이 곳으로 가서
+  // 계속한다.  이 노드에 대응되는 문자열의 접미사이면서 트라이에
+  // 포함된 최대 문자열
+  TrieNode* fail;
+  // 출력 문자열 목록: 이노드가 방문되었을 때 등장하는 바늘 문자열들의
+  // 번호
+  std::vector<int> output;
+  
+  TrieNode() : terminal(-1), fail(NULL) {
     memset(children, 0, sizeof(children));
   }
   ~TrieNode() {
@@ -21,14 +33,14 @@ struct TrieNode {
         delete children[i];
   }
   // 이 노드를 루트로 하는 트라이에 문자열 key를 추가한다.
-  void insert(const char* key) {
+  void insert(const char* key, int order) {
     if (*key == 0) {
-      terminal = true;
+      terminal = order;
     } else {
       int next = ToNumber(*key);
       if (children[next] == NULL)
         children[next] = new TrieNode();
-      children[next]->insert(key + 1);
+      children[next]->insert(key + 1, order);
     }
   }
 
@@ -42,6 +54,46 @@ struct TrieNode {
   }
 };
 
+// 트라이가 주어질 때 각 노드에 대해 실패 연결과 출력 문자열 목록을
+// 계산한다.
+void ComputeFailFunc(TrieNode* root) {
+  // 루트에서부터 시작해 한 단계씩 아래로 내려가며 각 노드의 실패
+  // 연결을 계산한다.
+  std::queue<TrieNode*> q;
+  // 루트의 실패 연결은 자기 자신
+  root->fail = root;
+  q.push(root);
+  // BFS
+  while (!q.empty()) {
+    TrieNode* here = q.front();
+    q.pop();
+    // here의 모든 자손에 대해 실패 연결을 계산하고 이들을 큐에
+    // 넣는다.
+    for (int edge = 0; edge < ALPHABETS; ++edge) {
+      TrieNode* child = here->children[edge];
+      if (!child) continue;
+      // 1레벨 노드의 실패 연결은 항상 루트
+      if (here == root)
+        child->fail = root;
+      else {
+        // 아닌 경우 부모의 실패 연결을 따라가면서 실패 연결을 찾는다.
+        TrieNode* t = here->fail;
+        while (t != root && t->children[edge] == NULL)
+          t = t->fail;
+        if (t->children[edge])
+          t = t->children[edge];
+        child->fail = t;
+      }
+      // 출력 문자열 목록: 실패 연결을 따라간 노드에서 복사해온 뒤
+      // 이 위치에서 끝나는 바늘 문자열이 있으면 추가한다.
+      child->output = child->fail->output;
+      if (child->terminal != -1)
+        child->output.push_back(child->terminal);
+      q.push(child);
+    }
+  }
+}
+
 std::vector<std::pair<int, int> > AhoCorasicSearch(
     const std::string& s, TrieNode* root) {
   std::vector<std::pair<int, int> > r;
@@ -54,18 +106,21 @@ std::vector<std::pair<int, int> > AhoCorasicSearch(
     if (state->children[chr])
       state = state->children[chr];
     for (int j = 0; j < state->output.size(); ++j)
-      r.push_back(make_pair(i, state->output[j]));
+      r.push_back(std::make_pair(i, state->output[j]));
   }  
   return r;
 }
 
 int main() {
+  // build trie
   TrieNode* ptn = new TrieNode();
-  ptn->insert("hello");
-  ptn->insert("world");
+  ptn->insert("hello", 0);
+  ptn->insert("world", 1);
+  ComputeFailFunc(ptn);
 
+  // search
   std::vector<std::pair<int, int> > r = AhoCorasicSearch(
       "ThisIsOurWorldHello", ptn);
-  
+
   delete ptn;
 }
