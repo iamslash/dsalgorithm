a536c44bdb77c0e579ff3e65f957e849b832d12f
iamslash <iamslash@gmail.com>
2016-12-15 15:47:30 +0900

    dfs java finished

--- graph/dfs/java/A.java
+++ graph/dfs/java/A.java
@@ -7,27 +7,33 @@ import java.util.*;
 // > java A
 public class A {
 
-  private int N;
+  private int N = 5;
   private ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-  private ArrayList<Integer> discovered;
+  private ArrayList<Integer> discovered = new ArrayList<Integer>();
   private int counter;
   
 
   private void BuildGraph(int n) {
     adj.clear();
-    adj.ensureCapacity(n);
+    for (int i = 0; i < n; ++i)
+      adj.add(new ArrayList<Integer>());
     discovered.clear();
-    discovered.ensureCapacity(n);
-    for (Integer i : discovered) discovered.set(i, -1);
+    for (int i = 0; i < n; ++i)
+      discovered.add(new Integer(-1));
     counter = 0;
-
+   
     adj.get(0).add(1);
     adj.get(1).add(2);
     adj.get(1).add(3);
-    adj.get(2).add(4);
+    adj.get(2).add(3);
   }
   
   private void Dfs(int here) {
+    System.out.print(String.format("%d ", here));
+    // for (Integer i : discovered)
+    //   System.out.print(String.format("%d ", i));
+    // System.out.println(String.format("> %d", counter));
+    
     discovered.set(here, counter++);
     for (int i = 0; i < adj.get(here).size(); ++i) {
       int there = adj.get(here).get(i);
