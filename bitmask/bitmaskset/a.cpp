365136a0316758fd6270de2050d0c09650916f67
iamslash <iamslash@gmail.com>
2016-12-21 15:05:38 +0900

    bitmask ing...

--- bitmask/bitmaskset/a.cpp
+++ bitmask/bitmaskset/a.cpp
@@ -1,20 +1,49 @@
 // Copyright (C) 2016 by iamslash
 
-include <cstdio>
+#include <cstdio>
+
+int BitCount(int x) {
+  if (x == 0) return 0;
+  return x % 2 + BitCount(x / 2);
+}
 
 int main()
 {
   // null set, full set
+  int zero_bitmask = 0;
+  int full_bitmask = (1 << 20) - 1;
   
   // add a element
+  int bitmask = 0;
+  int n = 2;
+  bitmask |= (1 << n);
+  
   // check a element included
-  // dell a element
+  if (bitmask & (1 << n))
+    printf("%d th element is in\n");
+  
+  // del a element
+  bitmask &= ~(1 << n);
+  
   // toggle a element
+  bitmask ^= (1 << n);
+  
   // set operation (added, intersection, removed, toggled)
+  int a = 0x01;
+  int b = 0x10;
+  int added_bitmask = (a | b);
+  int inter_bitmask = (a & b);
+  int remov_bitmask = (a & ~b);
+  int toggl_bitmask = (a ^ b);
+  
   // size of set (__builtin_popcount())
+  printf("size is %d\n", __builtin_popcount(a));
+  printf("size is %d\n", BitCount(a));
+  
   // find a min element (__builtin_ctz())
+  
   // del a min element
+
   // traversal of set  
   
-    printf("hello world\n");
 }
--- bitmask/priorityqueue/a.cpp
+++ bitmask/priorityqueue/a.cpp
@@ -17,11 +17,15 @@ void pq_push(int i) {
 int pq_pop() {
   int r = -1;
   for (int i = 0; i < 3; ++i) {
-    int n = __builtin_ctz(pqueue[i]);
-    if (n < 64) {
-      r = i * 64 + n;
-      pqueue[i] &= ~(1LL << n);
-      break;
+    // __builtin_ctz returns 0 or 64 pqueue[i] is zero
+    if (pqueue[i] > 0) {
+      int n = __builtin_ctz(pqueue[i]);
+      // printf(" [%2d] %2d\n", i, n);
+      if (n < 64) {
+        r = i * 64 + n;
+        pqueue[i] &= ~(1LL << n);
+        break;
+      }
     }
   }
   return r;
@@ -30,26 +34,32 @@ int pq_pop() {
 void pq_print() {
   for (int i = 2; i >= 0; --i) {
     for (int j = 63; j >= 0; --j) {
-      printf("%1d ", pqueue[i] & (1LL << j) ? 1 : 0 );
+      printf("%1d", pqueue[i] & (1LL << j) ? 1 : 0 );
     }
     printf(" | ");
   }
   printf("\n");
+  // for (int i = 2; i >= 0; --i) {
+  //   printf("%x | %x | %x\n", pqueue[2], pqueue[1], pqueue[0]);
+  // }
 }
 
 int main()
 {
-  memset(pqueue, 0LL, sizeof(pqueue));
+  // memset(pqueue, 0LL, sizeof(pqueue));
+  for (int i = 0; i < 3; ++i) {
+    pqueue[i] = 0;
+  }
   
-  // pq_push(5);
-  // pq_push(100);
-  // printf("popped %4d\n", pq_pop());
-  // printf("popped %4d\n", pq_pop());
-  // pq_print();
-
+  pq_push(0);
+  pq_push(1);
+  printf("popped %4d\n", pq_pop());
+  printf("popped %4d\n", pq_pop());
+  printf("popped %4d\n", pq_pop());
+  pq_print();
   
-  // test for __builtin_ctzll
-  // printf("%d\n", __builtin_ctzll(0LL));
-  // printf("%d\n", __builtin_ctzll(1LL));
-  // printf("%d\n", __builtin_ctzll(8LL));
+  // // test for __builtin_ctzll
+  // printf("%d\n", __builtin_ctzll(0LL)); // 64
+  // printf("%d\n", __builtin_ctzll(1LL)); // 0
+  // printf("%d\n", __builtin_ctzll(8LL)); // 3
 }
