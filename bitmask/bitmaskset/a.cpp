8ca088ebb484ba43d4e8e200128863da7aa5123a
iamslash <iamslash@gmail.com>
2016-12-21 16:05:20 +0900

    bitmaskset finished

--- bitmask/bitmaskset/a.cpp
+++ bitmask/bitmaskset/a.cpp
@@ -7,6 +7,13 @@ int BitCount(int x) {
   return x % 2 + BitCount(x / 2);
 }
 
+void PrintBitmask(int bitmask) {
+  for (int j = 31; j >= 0; --j) {
+    printf("%1d", bitmask & (1LL << j) ? 1 : 0 );
+  }
+  printf("\n");
+}
+
 int main()
 {
   // null set, full set
@@ -18,32 +25,39 @@ int main()
   int n = 2;
   bitmask |= (1 << n);
   
-  // check a element included
-  if (bitmask & (1 << n))
-    printf("%d th element is in\n");
+  // // check a element included
+  // if (bitmask & (1 << n))
+  //   printf("%d th element is in\n");
   
-  // del a element
-  bitmask &= ~(1 << n);
+  // // del a element
+  // bitmask &= ~(1 << n);
   
-  // toggle a element
-  bitmask ^= (1 << n);
+  // // toggle a element
+  // bitmask ^= (1 << n);
   
-  // set operation (added, intersection, removed, toggled)
-  int a = 0x01;
-  int b = 0x10;
-  int added_bitmask = (a | b);
-  int inter_bitmask = (a & b);
-  int remov_bitmask = (a & ~b);
-  int toggl_bitmask = (a ^ b);
+  // // set operation (added, intersection, removed, toggled)
+  // int a = 0x01;
+  // int b = 0x10;
+  // int added_bitmask = (a | b);
+  // int inter_bitmask = (a & b);
+  // int remov_bitmask = (a & ~b);
+  // int toggl_bitmask = (a ^ b);
   
-  // size of set (__builtin_popcount())
-  printf("size is %d\n", __builtin_popcount(a));
-  printf("size is %d\n", BitCount(a));
+  // // size of set (__builtin_popcount())
+  // printf("size is %d\n", __builtin_popcount(a));
+  // printf("size is %d\n", BitCount(a));
   
-  // find a min element (__builtin_ctz())
+  // // find a min element (__builtin_ctz())
+  // printf("first element is at %d\n", __builtin_ctz(a));
+  // printf("first element is at %d\n", (a & -a));
   
-  // del a min element
+  // // del a min element
+  // a &= (a - 1);
 
-  // traversal of set  
-  
+  // traversal of set
+  bitmask = 0x08;
+  bitmask = 0x15;
+  for (int subset = bitmask; subset; subset = ((subset - 1) & bitmask)) {
+    PrintBitmask(subset);
+  }
 }
