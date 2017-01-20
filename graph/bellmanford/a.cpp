def2227941800ca68c412bbc51c3b625666d2244
iamslash <iamslash@gmail.com>
2017-01-20 16:07:22 +0900

    bellman ford finished

--- graph/bellmanford/a.cpp
+++ graph/bellmanford/a.cpp
@@ -7,8 +7,8 @@
 #include <cstdlib>
 #include <limits>
 
-#define MAX_V 16
-const int MAX_INT = std::numeric_limits<int>::max();
+#define MAX_V 6
+const int MAX_INT = 987654321;
 
 int V = MAX_V;
 
@@ -18,20 +18,21 @@ std::vector<int> BellmanFord(int src) {
   std::vector<int> upper(V, MAX_INT);
   upper[src] = 0;
   bool updated;
-  for (int iter = 0; iter < V; ++iter) {
+  for (int iter = 0; iter < V; ++iter) {    
     updated = false;
-
-    for (int herenode = 0; herenode < V; ++iter) {
+    for (int herenode = 0; herenode < V; ++herenode) {
+      // printf("%d\n", herenode);
       for (int i = 0; i < adj[herenode].size(); ++i) {
         int therenode = adj[herenode][i].first;
         int therecost = adj[herenode][i].second;
+        
         if (upper[therenode] > upper[herenode] + therecost) {
           upper[therenode] = upper[herenode] + therecost;
           updated = true;
+          printf(" %d -> %d %d\n", herenode, therenode, upper[therenode]);
         }
       }
     }
-
     if (!updated)
       break;
   }
@@ -46,34 +47,19 @@ int main() {
   for (int i = 0; i < MAX_V; ++i)
     adj[i].clear();
 
-  adj[0].push_back(std::make_pair(1, 5));
-  adj[0].push_back(std::make_pair(2, 1));
-
-  adj[1].push_back(std::make_pair(0, 5));
-  adj[1].push_back(std::make_pair(3, 1));
-  adj[1].push_back(std::make_pair(5, 3));
-  adj[1].push_back(std::make_pair(6, 3));
-
-  adj[2].push_back(std::make_pair(0, 1));
-  adj[2].push_back(std::make_pair(3, 2));
-
-  adj[3].push_back(std::make_pair(1, 1));
-  adj[3].push_back(std::make_pair(2, 2));
-  adj[3].push_back(std::make_pair(4, 5));
-  adj[3].push_back(std::make_pair(6, 3));
-
-  adj[4].push_back(std::make_pair(3, 5));
+  adj[0].push_back(std::make_pair(1, 10));
+  adj[0].push_back(std::make_pair(5, 8));
+  adj[1].push_back(std::make_pair(3, 2));
+  adj[2].push_back(std::make_pair(1, 1));
+  adj[3].push_back(std::make_pair(2, -2));
+  adj[4].push_back(std::make_pair(1, 2));
+  adj[4].push_back(std::make_pair(3, -1));
+  adj[5].push_back(std::make_pair(4, 1));
   
-  adj[5].push_back(std::make_pair(1, 3));
-  adj[5].push_back(std::make_pair(6, 2));
-  
-  adj[6].push_back(std::make_pair(1, 3));
-  adj[6].push_back(std::make_pair(3, 3));
-  adj[6].push_back(std::make_pair(5, 2));
-      
   std::vector<int> r = BellmanFord(0);
 
-  // PrintVInt(r);  
+  // PrintVInt(r);
+  
   
   return 0;
 }
