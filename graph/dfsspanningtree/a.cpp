e4e3cd0838b4cc58bce8cf89c43926e940b31eb8
iamslash <iamslash@gmail.com>
2017-02-10 18:16:23 +0900

    dfs spanning tree ing...

--- graph/dfsspanningtree/a.cpp
+++ graph/dfsspanningtree/a.cpp
@@ -6,13 +6,10 @@
 #include <limits>
 #include <cstring>
 
-int N = 5;
+int N = 4;
 
-// 그래프의 인접 행렬
-// adj[i][j] = i와 j사이의 간선의 수
 std::vector<std::vector<int> > adj;
 std::vector<int> discovered, finished;
-// 지금까지 발견한 정점의 수
 int counter = 0;
 
 void PrintVInt(const std::vector<int>& v) {
@@ -22,31 +19,7 @@ void PrintVInt(const std::vector<int>& v) {
 }
 
 void DfsSpanningTree(int here) {
-  discovered[here] = counter++;
-  // 모든 인접 정점을 순회하면서
-  for (int i = 0; i < adj[here].size(); ++i) {
-    if (adj[here][i] == 0)
-      continue;
-    
-    int there = i;
-
-    printf("(%d, %d) is a ", here, there);
-    // 아직 방문한 적 없다면 방문한다.
-    if (discovered[there] == -1) {
-      printf("tree edge\n");
-      DfsSpanningTree(there);
-    // 만약 there가 here보다 늦게 발견됐으면 there는 here의 후손이다.
-    } else if (discovered[here] < discovered[there]) {
-      printf("forward edge\n");
-    // 만약 DfsSpanningTree(there)가 종료되지 않았다면 here는 there의 후손이다.
-    } else if (finished[there] == 0) {
-      printf("back edge\n");
-    // 그렇지 않으면 cross edge이다.
-    } else {
-      printf("cross edge\n");
-    }
-  }
-  finished[here] = 1;
+  
 }
 
 int main() {
