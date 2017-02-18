b5c73e4e419aa493518be23789a7c7ed94398f80
iamslash <iamslash@gmail.com>
2017-02-18 21:00:56 +0900

    suffix array naive added

---
+++ string/suffixarraynaive/a.cpp
@@ -0,0 +1,47 @@
+// Copyright (C) 2016 by iamslash
+
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cstring>
+
+void PrintVInt(const std::vector<int>& a) {
+  for (int i=0; i < a.size(); ++i) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
+
+void PrintSuffixArray(const std::string& h, const std::vector<int>& sa) {
+  for (int i = 0; i < sa.size(); ++i) {
+    printf("%s\n", h.c_str() + sa[i]);
+  }
+}
+
+struct Comp {
+  std::string m_s;
+  explicit Comp(std::string s) {
+    m_s = s;
+  }
+  bool operator() (int i, int j) {
+    printf("  %d %d\n", i, j);
+    return strcmp(m_s.c_str() + i, m_s.c_str() + j) < 0;
+  }
+};
+
+std::vector<int> GetSuffixArray(const std::string& s) {
+  std::vector<int> r(s.size());
+  for (int i = 0; i < s.size(); ++i) {
+    r[i] = i;
+  }
+  std::sort(r.begin(), r.end(), Comp(s));
+  return r;
+}
+
+int main() {
+  std::string h = "mississipi";
+  std::vector<int> r = GetSuffixArray(h);
+  PrintSuffixArray(h, r);
+  return 0;
+}
