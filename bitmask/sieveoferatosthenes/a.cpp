5a8659abfc34501cf7df42a078c8a0e5fe36819d
iamslash <iamslash@gmail.com>
2017-02-22 07:55:05 +0900

    sieve of eratosthenes finished

--- bitmask/sieveoferatosthenes/a.cpp
+++ bitmask/sieveoferatosthenes/a.cpp
@@ -9,17 +9,51 @@
 
 unsigned char SIEVE[(MAX_N + 7) / 8];
 
-void print_sieve(const int& n) {
+bool is_prime(const int& n) {
+  int div = n / 8;
+  int mod = n % 8;
+  return (SIEVE[div] & (1 << mod)) ? true : false;;
+}
 
+void set_composite(const int& n) {
+  int div = n / 8;
+  int mod = n % 8;
+  SIEVE[div] &= ~(1 << mod);
+}
+
+void print_sieve(const int& n) {
+  for (int i = 0; i < n; ++i) {
+    if (is_prime(i))
+      printf("%d ", i);
+  }
+  printf("\n");
 }
 
 void eratos_thenes(const int& n) {
+  set_composite(0);
+  set_composite(1);
 
+  // i = p * q ( p <= q)
+  // p <= sqrt(i)
+  // j is from i * i to n - 1
+  // 
+  int sqrtn = sqrt(n);
+  for (int i = 2; i < sqrtn; ++i) {
+    if (is_prime(i)) {
+      printf("  %d\n", i);
+      for (int j = i*i; j < n; j+=i) {
+        printf("%d ", j);
+        set_composite(j);
+      }
+      printf("\n");
+    }
+  }
 }
 
 int main() {
-  int n = 1024;
-  eratos_thenes(n);
-  print_sieve(n);
+  memset(SIEVE, 255, sizeof(SIEVE));
+  int N = 64;
+  eratos_thenes(N);
+  print_sieve(N);
   return 0;
 }
