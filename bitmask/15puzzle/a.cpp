84109c40ebaa25bee30c2a52373d8fdcec6f595a
iamslash <iamslash@gmail.com>
2017-02-22 08:08:59 +0900

    15 puzzle ing...

--- bitmask/15puzzle/a.cpp
+++ bitmask/15puzzle/a.cpp
@@ -5,22 +5,25 @@
 
 // get the value which is wrote on mask at index
 // index is 15 puzzle's index
-int get(uint64_t mask, int index) {
-  return (mask >> (index << 2)) & 15;
+int get_15(uint64_t mask, int index) {
+  return (mask >> (index << 4)) & 0x0f;
 }
 
 // set the value which is on mask at index
-uint64_t set(uint64_t mask, int index, uint64_t value) {
-  return mask & ~(15LL << (index << 2)) | (value << (index << 2));
+uint64_t set_15(uint64_t mask, int index, uint64_t value) {
+  uint64_t r;
+  
+  return r;
 }
 
 int main()
 {
-  uint64_t puzzle = 0x00;
-  for (int i = 0; i < 16; ++i) {
-    puzzle = set(puzzle, i, i);
-  }
-  for (int i = 0; i < 16; ++i) {
-    printf("[%2d] %2d\n", i, get(puzzle, i));
-  }
+  printf("%d\n", get_15(0x30, 1));
+  // uint64_t puzzle = 0x00;
+  // for (int i = 0; i < 16; ++i) {
+  //   puzzle = set_15(puzzle, i, i);
+  // }
+  // for (int i = 0; i < 16; ++i) {
+  //   printf("[%2d] %2d\n", i, get_15(puzzle, i));
+  // }
 }
